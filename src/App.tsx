fa25bb15f00566fe1751559284592fdbdfaa5f89
Yusuf Erdem Aykaç <yusuferdemaykacc@gmail.com>
2026-03-24 09:18:40 +0300

    chore : initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App(){
+  return (
+    <div> 
+      <h1> Web Tasarimi Ve Programlama </h1>
+      <h2> LAB - 1 </h2>
+      <p> Ad Soyad : Yusuf Erdem Aykaç</p>
+      <p> Ogrenci No : 230541001</p>
+    </div>
+  )
+}
+
+export default App;
